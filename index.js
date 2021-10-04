eec6e0872f1444a5be47e0f5eb18159bb2c92950
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-04 13:49:00 +1100

    Completed Assigment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+let num1=31
+let num2=2
+let multiply;
+multiply=num1*num2;
+console.log(multiply);
+
+let random;
+random=Math.ceil(Math.random());
+console.log(random);
+
+let num3=10;
+let num4=6;
+let mod;
+mod=num3%num4;
+console.log(mod);
+
+let max;
+max=Math.max(10,20,2,5,6,7,8);
+console.log(max);
